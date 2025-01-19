65fdd1d2c4626c42da6eb1fcd96734cbb1710827
zvdv <156561301+zvdv@users.noreply.github.com>
2025-01-19 00:57:23 -0800

    Added message title

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -129,7 +129,7 @@ class BuddyChat implements vscode.WebviewViewProvider {
 	public addAIMessage(message: string) {
 		if (this._view) {
 			this._view.show?.(true)
-			this._view.webview.postMessage({ user: "AI Buddy", message: message})
+			this._view.webview.postMessage({ message: {user: "AI Buddy", message: message}})
 		}
 	}
 
