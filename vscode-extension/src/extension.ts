8ad6c8b0bac7aea5a0b3e1d10463099a38056b9b
zvdv <156561301+zvdv@users.noreply.github.com>
2025-01-18 19:09:22 -0800

    vscode: Display HTML in view

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -10,6 +10,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "test" is now active!');
 
+	const provider = new BuddyChat(context.extensionUri);
+	context.subscriptions.push(vscode.window.registerWebviewViewProvider(BuddyChat.viewType, provider));
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -82,3 +85,75 @@ async function getTerminalOutput(): Promise<string | null> {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+class BuddyChat implements vscode.WebviewViewProvider {
+	public static readonly viewType = 'buddy-chat';
+
+	private _view?: vscode.WebviewView;
+	
+	constructor(private readonly _extensionUri: vscode.Uri){
+
+	}
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		_context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
+
+			localResourceRoots: [
+				this._extensionUri
+			]
+		};
+
+		//webviewView.webview.html = '<p>Hi!</p>';
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+		// webviewView.webview.onDidReceiveMessage(data => {
+		// 	switch (data.type) {
+		// 		case 'colorSelected':
+		// 			{
+		// 				vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+		// 				break;
+		// 			}
+		// 	}
+		// });		
+	}
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+
+		// Do the same for the stylesheet.
+		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
+		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
+		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+	
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading styles from our extension directory,
+					and only allow scripts that have a specific nonce.
+					(See the 'webview-sample' extension sample for img-src content security policy examples)
+				-->
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				
+
+				<title>AI Buddy</title>
+			</head>
+			<body>
+				<p>Hi</p>
+			</body>
+			</html>`;
+	}
+}
